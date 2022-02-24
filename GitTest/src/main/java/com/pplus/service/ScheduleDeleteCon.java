aa03e39c3761525a90fbad5b28d7f47e143d5fa9
smhrd <smhrd@210.183.87.89>
2022-02-24 17:10:25 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/2022-SMHRD-KDT-BigData-1/Pplus.git

--- GitTest/src/main/java/com/pplus/service/ScheduleDeleteCon.java
+++ GitTest/src/main/java/com/pplus/service/ScheduleDeleteCon.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.pplus.model.PMemberDTO;
+import com.pplus.model.ScheduleDAO;
 
 
 @WebServlet("/ScheduleDeleteCon")
@@ -18,16 +19,29 @@ public class ScheduleDeleteCon implements iPCommand {
 	
 	public void execute(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-	
+		
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8"); 
 		HttpSession session = request.getSession();
 		
 		PMemberDTO member = (PMemberDTO)session.getAttribute("member");
+		int cnt=0;
+		String nick = member.getMember_nick();
+		ScheduleDAO scheduleDAO =new ScheduleDAO();
 		
-		String[] list = request.getParameterValues("list");
+		String[] scheduleNumList = request.getParameterValues("scheduleNumList");
+		int size = scheduleNumList.length;
+		for(int i=0; i<size;i++) {
+			cnt += scheduleDAO.scheduleDelete(nick, Integer.parseInt(scheduleNumList[i]));
+		}
 		
-		System.out.println(list[0]);
+		if(cnt>0) {
+			System.out.println("삭제성공");
+			response.sendRedirect("scheduleindex.jsp");
+		} else {
+			System.out.println("삭제실패");
+			response.sendRedirect("scheduleindex.jsp");
+		}
 		
 	}
 
